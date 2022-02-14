dd20cc2cb6538c695d0fa2b3e59b7a54caa6633b
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-14 13:07:50 -0300

    tentativa bonus 1

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,33 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  let resultado ;
+  switch (number) {
+    case number.length !== 11: 
+      console.log("Array com tamanho incorreto.");
+      break;
+    case number < 0 :
+      for  (i = 0; index > number.lenght ; i++){
+      if (number[index] < 0){
+         resultado = console.log(("não é possível gerar um número de telefone com esses valores" ))
+      }
+    }
+      break;
+    case number > 9: 
+    for  (i = 0; index < number.lenght ; i++){
+      if (number[index] > 9){
+          console.log("não é possível gerar um número de telefone com esses valores" )
+      }
+    }
+      break;
+    case number.length > 11: 
+      console.log("Array com tamanho incorreto.");
+      break;
+
+    default:
+      console.log( "não é possível gerar um número de telefone com esses valores");
+      break; 
+    }
+    return resultado 
 }
 
 // Desafio 12
